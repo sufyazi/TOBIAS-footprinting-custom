754789d86693bdb93acf4f79bb170c6ae9e3c210
sufyazi <sufyazi@outlook.com>
2024-01-17 11:26:31 +0800

    update script

--- scripts/preprocess-bam_input_transfer-ODIN-v2.sh
+++ scripts/preprocess-bam_input_transfer-ODIN-v2.sh
@@ -131,13 +131,13 @@ for dataset in "${datasets[@]}"; do
 done
 
 # transfer the sample dir to remote server
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/nscc_id_rsa
-if rsync -havPz "/home/msazizan/cargospace/tobias-bam-input/" suffiazi@aspire2antu.nscc.sg:/home/users/ntu/suffiazi/scratch/inputs/tobias-bam-input/; then
-	echo "Datasets have been transferred to Aspire."
-	echo "Input bam file collation, merging,and transfer to remote server have been completed."
-else
-	echo "Dataset transfer was interrupted."
-fi
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/nscc_id_rsa
+# if rsync -havPz "/home/msazizan/cargospace/tobias-bam-input/" suffiazi@aspire2antu.nscc.sg:/home/users/ntu/suffiazi/scratch/inputs/tobias-bam-input/; then
+# 	echo "Datasets have been transferred to Aspire."
+# 	echo "Input bam file collation, merging,and transfer to remote server have been completed."
+# else
+# 	echo "Dataset transfer was interrupted."
+# fi
 
 
