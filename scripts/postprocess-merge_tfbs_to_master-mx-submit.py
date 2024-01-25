1219f298d96d034031f61dfe0c80ccca13a219ed
sufyazi <sufyazi@outlook.com>
2024-01-25 17:43:31 +0800

    optimize logical flow

--- scripts/postprocess-merge_tfbs_to_master-mx-submit.py
+++ scripts/postprocess-merge_tfbs_to_master-mx-submit.py
@@ -67,20 +67,27 @@ print(f"Number of subdirectories: {len(motif_dir)}")
 
 # Loop through the subdirectories
 for counter, motif in enumerate(motif_dir, start=1):
-    # check if the merged dataframe file exists
-    if os.path.exists(f'{output_directory}/{motif}_tfbs_merged_matrix-full.parquet'):    
-        print(f"File of {motif} TFBS merged matrix already exists. Skipping {motif}...")
-    else:
-        if counter == 20:
-            print("Current job count has reached 20. Submission limiter will henceforth be applied to 10 jobs per 15 minutes.")
-        
-        # check if the current counter is a multiple of 10
-        if counter > 20 and counter % 10 == 0:
+
+    if counter == 20:
+        print("Current job count has reached 20. Submission limiter will henceforth be applied to 10 jobs per 15 minutes.")
+    
+    # check if the current counter is a multiple of 10
+    if counter > 20 and counter % 10 == 0:
+        # check if the merged dataframe file exists
+        if os.path.exists(f'{output_directory}/{motif}_tfbs_merged_matrix-full.parquet'):    
+            print(f"File of {motif} TFBS merged matrix already exists. Skipping {motif}...")
+            continue
+        else:
             print("Counter is at the limiter mark. Sleeping for 15 minutes...")
             time.sleep(900)
             print("15 minutes have passed. Resuming submission...")
-        
-        # submit job to cluster
+    
+    # submit job to cluster
+    # check if the merged dataframe file exists
+    if os.path.exists(f'{output_directory}/{motif}_tfbs_merged_matrix-full.parquet'):    
+        print(f"File of {motif} TFBS merged matrix already exists. Skipping {motif}...")
+        continue
+    else:
         print(f"File of {motif} TFBS merged matrix does not exist. Proceeding...")
         print(f"Processing {motif}...")
         if id_list_file:
